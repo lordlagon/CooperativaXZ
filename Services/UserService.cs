edcb633cbd9b8276b444ef854dd879b7db2c0bf8
Andre Xavier <lordlagon@gmail.com>
2019-10-12 20:40:30 -0300

    Inclusao UserService

---
+++ Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CooperativaXZ
+{
+    public class UserService
+    {
+        private readonly IJwtService jwtService;
+        private readonly IApiService api;
+        private readonly StateService state;
+    }
+}
