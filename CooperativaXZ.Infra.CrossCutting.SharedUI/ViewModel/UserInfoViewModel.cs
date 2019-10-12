bedb8adad5be0be875cb60e34647679a07fd8634
Andre Xavier <lordlagon@gmail.com>
2019-10-12 17:10:19 -0300

    inclusão Infra.Crosscutting

---
+++ CooperativaXZ.Infra.CrossCutting.SharedUI/ViewModel/UserInfoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CooperativaXZ.Infra.CrossCutting.SharedUI.ViewModel
+{
+    public class UserInfoViewModel
+    {
+        public bool IsAuthenticated { get; set; }
+        public string UserName { get; set; }
+        public Dictionary<string, string> ExposedClaims { get; set; }
+    }
+}
