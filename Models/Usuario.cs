0881e8ba88d0c4d69faf03631518caff4a7b9188
Andre Xavier <lordlagon@gmail.com>
2019-10-12 20:30:18 -0300

    Removendo pastas desnecessárias

---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CooperativaXZ.Models
+{
+    public class Usuario
+    {
+        public string name { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string cpf { get; set; }
+    }
+
+}
