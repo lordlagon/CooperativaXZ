5321d70480ce9007c3270bd67aea075e0dfb8287
Andre Xavier <lordlagon@gmail.com>
2019-10-12 21:21:04 -0300

    ADD JWT

---
+++ Services/IJwtService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CooperativaXZ.Services
+{
+    public interface IJwtService
+    {
+        Task<string> GetTokenAsync();
+        Task<bool> SaveTokenAsync(string Token);
+        Task<bool> DestroyTokenAsync();
+    }
+}
