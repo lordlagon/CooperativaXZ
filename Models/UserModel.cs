bc2aa027426396f98d290d8f77b377761e446293
Andre Xavier <lordlagon@gmail.com>
2019-10-12 23:23:34 -0300

    Include Models

---
+++ Models/UserModel.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CooperativaXZ
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Token { get; set; }
+    }
+
+    public class UserCredentials
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Username { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; }
+        [JsonProperty(PropertyName = "cpf")]
+        public string Cpf { get; set; }
+    }
+
+}
