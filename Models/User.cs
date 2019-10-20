bf2a5ab5d7db4af1d072e1459effe8806cb6456c
João Vitor <vitorjoao2112@hotmail.com>
2019-10-20 20:55:48 -0300

    adicao User com biding

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json;
+
+namespace CooperativaXZ
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string Token { get; set; }
+    }
+}
