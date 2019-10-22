1821c345b9f1b0d7a96ffcfac3d014ec9bb6e915
Andre Xavier <lordlagon@gmail.com>
2019-10-22 20:56:10 -0300

    referenciando cadastroService

--- Startup.cs
+++ Startup.cs
@@ -14,20 +14,18 @@ namespace CooperativaXZ
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+
+            services.AddTransient<IJwtService, JwtService>();
+            services.AddTransient<IConsoleLogService, ConsoleLogService>();
+            
+
+            services.AddScoped<IApiService, ApiService>();
+            services.AddScoped<System.Net.Http.HttpClient>();
+            services.AddScoped<UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,12 +37,12 @@ namespace CooperativaXZ
             }
             else
             {
-                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
