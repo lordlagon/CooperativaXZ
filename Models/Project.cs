70e0761aeec3aa261a51d72761adbb847fb0f0bb
Andre Xavier <lordlagon@gmail.com>
2019-10-25 22:20:30 -0300

    class project

--- Models/Project.cs
+++ Models/Project.cs
@@ -7,6 +7,10 @@ namespace CooperativaXZ
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string OfferValue { get; set; }
+        public string Analyze { get; set; }
         public string Hirer { get; set; }
         public ProjectDelivere[] ProjectDeliveries { get; set; }
         public ProjectEvaluation[] ProjectEvaluations { get; set; }
