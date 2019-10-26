f4be66f9ee389eedb31d41babd27ab68a7b87317
Andre Xavier <lordlagon@gmail.com>
2019-10-26 00:15:59 -0300

    projects

--- Models/Project.cs
+++ Models/Project.cs
@@ -5,14 +5,23 @@ namespace CooperativaXZ
 {
     public class Project
     {
+        [JsonProperty("_id")]
         public string Id { get; set; }
+        [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("description")]
         public string Description { get; set; }
+        [JsonProperty("scope")]
         public string Scope { get; set; }
+        [JsonProperty("offer_value")]
         public string OfferValue { get; set; }
+        [JsonProperty("analyze")]
         public string Analyze { get; set; }
+        [JsonProperty("hirer")]
         public string Hirer { get; set; }
+        [JsonProperty("project_deliveries")]
         public ProjectDelivere[] ProjectDeliveries { get; set; }
+        [JsonProperty("project_evaluations")]
         public ProjectEvaluation[] ProjectEvaluations { get; set; }
     }
 
